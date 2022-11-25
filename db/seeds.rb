a426e4d7e0e4becc41532087ef5b5c9187d70e4b
Linah Imbari <linahmuhonjaimbari@gmail.com>
2022-11-25 03:16:50 +0300

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
+# Pass1 = Passenger.create(name: Faker::Name.name)
+# Pass2 = Passenger.create(name: Faker::Name.name)
+# Pass3 = Passenger.create(name: Faker::Name.name)
+# Pass4 = Passenger.create(name: Faker::Name.name)
+
+# taxi1 = Taxi.create(size:Faker::Esport.player)
+# taxi2 = Taxi.create(size:Faker::Esport.player)
+# taxi3 = Taxi.create(size:Faker::Esport.player)
+# taxi4 = Taxi.create(size:Faker::Esport.player)
+
+# ride1 = Ride.create(taxi_id: taxi1, passenger_id: pass2)
+# ride2 = Ride.create(taxi_id: taxi3, passenger_id: pass1)
+# ride3 = Ride.create(taxi_id: taxi4, passenger_id: pass3)
+# ride4 = Ride.create(taxi_id: taxi2, passenger_id: pass2)
+# ride5 = Ride.create(taxi_id: taxi1, passenger_id: pass2)
+# ride6 = Ride.create(taxi_id: taxi1, passenger_id: pass4)
+# ride7 = Ride.create(taxi_id: taxi4, passenger_id: pass3)
+
